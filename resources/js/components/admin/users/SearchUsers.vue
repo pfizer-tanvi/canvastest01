78da69235156a79383101143f260730aba30c94a
pfizer-tanvi <91727430+pfizer-tanvi@users.noreply.github.com>
2022-01-21 16:47:55 +0530

    Initialize snyk-upgrade-70536345d19b87ce762fc42336981c25

---
+++ resources/js/components/admin/users/SearchUsers.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <b-form-input
+      :autofocus="true"
+      type="text"
+      v-model="searchInput"
+      @input="searchEvent"
+      placeholder="Search Users"
+    ></b-form-input>
+    Searching for {{ searchInput }}
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    searchEvent(search) {
+      this.$emit("search", search);
+    },
+  },
+  data() {
+    return {
+      searchInput: null,
+    };
+  },
+};
+</script>
