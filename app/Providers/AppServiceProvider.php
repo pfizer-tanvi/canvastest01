2aef1ce851df67d074bf84e8cc2b1524fc3df552
pfizer-tanvi <91727430+pfizer-tanvi@users.noreply.github.com>
2022-01-21 16:47:30 +0530

    Initialize snyk-upgrade-ed1a9c26213c57ff9a7a5a7eb0754634

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
+
+/**
+ * @codeCoverageIgnore
+ */
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Paginator::useBootstrap();
+
+        $this->app->bind("App\Http\Controllers\Auth\ForgotPasswordController", function ($app) {
+            return true;
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
+        Schema::defaultStringLength(191);
+    }
+}
