3c14ff628db0ec838ede4f65faa0c3b97f46c1e0
pfizer-tanvi <91727430+pfizer-tanvi@users.noreply.github.com>
2022-01-21 16:48:24 +0530

    Initialize snyk-upgrade-86069302ebb291a8b050fbc42d797d23

---
+++ aws/cfn-cli-command.sh
@@ -0,0 +1,6 @@
+aws cloudformation create-stack --stack-name APP_NAME \
+--template-body file:///$(pwd)/aws/web-template.yml \
+--tags file:///$(pwd)/aws/tag.json \
+--parameters file:///$(pwd)/aws/parameters.json \
+--capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+--role-arn arn:aws:iam::364215618558:role/service/cloudformation-builder-role
