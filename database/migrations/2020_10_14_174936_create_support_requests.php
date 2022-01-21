e3cf561672802871374b46e099de5af6c790f804
pfizer-tanvi <91727430+pfizer-tanvi@users.noreply.github.com>
2022-01-21 16:49:12 +0530

    Initialize snyk-upgrade-75ab09619a2264bd55404291cc76e2ed

---
+++ database/migrations/2020_10_14_174936_create_support_requests.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupportRequests extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('support_requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("title", 255);
+            $table->longText("description", 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('support_requests');
+    }
+}
---
+++ resources/js/components/utility/FieldError.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="alert alert-danger" v-if="hasErrorsForField">
+          <span v-for="(error, repo) in errors" :key="repo" v-if="repo == field">
+            <ul>
+                <li v-for="(item, index) in error" :key="index">
+                    {{ item }}
+                </li>
+            </ul>
+          </span>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['errors', 'field'],
+  computed: {
+    hasErrorsForField() {
+      return !_.isEmpty(this.errors) && this.errors[this.field] !== undefined;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
