4982a4cd15b961b8df89cb725a27553e70e9b47e
pfizer-tanvi <91727430+pfizer-tanvi@users.noreply.github.com>
2022-01-21 16:47:40 +0530

    Initialize composite

---
+++ resources/js/components/admin/users/UsersIndex.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <b-table striped hover :fields="fields" :items="users.data">
+      <template #cell(is_super_admin)="data">
+        {{ data.value | yesNo }}
+      </template>
+      <template #cell(created_at)="data">
+        {{ data.value | datetime }}
+      </template>
+      <template #cell(updated_at)="data">
+        {{ data.value | datetime }}
+      </template>
+      <template #cell(edit)="data">
+        <b-button @click="edit(data.item)">edit</b-button>
+      </template>
+    </b-table>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["users"],
+  methods: {
+    edit(user) {
+      this.$emit("editButtonClicked", user);
+    },
+  },
+  data() {
+    return {
+      fields: [
+        "id",
+        {
+          key: "Name",
+          sortable: true,
+        },
+        "email",
+        "is_super_admin",
+        "created_at",
+        "updated_at",
+        {
+          key: "edit",
+          label: "Edit",
+        },
+      ],
+    };
+  },
+};
+</script>
