21715641a0b3f78c14c7277a130259f53537aa92
pfizer-tanvi <91727430+pfizer-tanvi@users.noreply.github.com>
2022-01-21 16:47:45 +0530

    Initialize snyk-upgrade-2bf40e164ab549d90932810af357f91f

---
+++ app/Http/Controllers/Media/MediaController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Media;
+
+use App\Http\Controllers\Controller;
+use App\Models\Media;
+use Illuminate\Http\Request;
+
+class MediaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Media::simplePaginate(25);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|file|max:4096',
+        ]);
+
+        $media = Media::fromUploadedFile($request->file('file'));
+
+        return response()->json($media);
+    }
+}
